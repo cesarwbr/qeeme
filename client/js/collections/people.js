d6de50056df2d08a4cfc1e16567c2116509d96d9
grafael <messaggeroo@gmail.com>
2014-10-08 19:14:05 -0300

    deploying in heroku

--- client/js/collections/people.js
+++ client/js/collections/people.js
@@ -1,6 +1,6 @@
 define(['backbone', '../models/person'], function(Backbone, Person) {
   return Backbone.Collection.extend({
     model: Person,
-    url: 'http://localhost:3000/'
+    url: 'http://192.168.56.101:3000/'
   });
 });
