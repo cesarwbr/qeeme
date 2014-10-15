6256504a454f0a06b11b4025cbec12a15228fdb0
Cesar William <cesarwbr@gmail.com>
2014-10-14 21:18:38 -0300

    Close drawer when select a history

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,5 @@
 require.config({
-  urlArgs: 'v=35',
+  urlArgs: 'v=36',
   paths: {
     'jquery': '../libs/jquery/dist/jquery.min',
     'underscore': '../libs/underscore/underscore-min',
--- client/js/router.js
+++ client/js/router.js
@@ -9,9 +9,13 @@ define(['backbone', 'views/app', 'views/search', 'views/history'], function(Back
     home: function(id) {
       var historyView = new HistoryView();
       historyView.render();
-      
+
       var searchView = new SearchView();
       searchView.render(this);
+
+      var mainPanel = $('#main-panel')[0];
+      mainPanel.closeDrawer();
+
       if (!!id) {
         new AppView(this, '/m/' + id);
       } else {
